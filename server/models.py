1ae163c886797a78265c6909e662248e80c19bc4
Stephen Muniu Kimani <stvnkimani@gmail.com>
2024-12-15 22:33:51 +0300

    adds models and routes for plant

--- server/models.py
+++ server/models.py
@@ -7,10 +7,21 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    name = db.Column(db.String(80), nullable=False)
+    image = db.Column(db.String(255), nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    is_in_stock = db.Column(db.Boolean, default=True)
 
-    def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+    # Optionally add more fields here, if needed.
+    # For example, description, category, etc.
+
+    # You can add the to_dict method directly in your model as an example:
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price,
+            'is_in_stock': self.is_in_stock
+        }
+
